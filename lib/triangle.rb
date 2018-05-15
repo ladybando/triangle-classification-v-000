77ecfa6cd07c89abfcac63a609a0663fdcd14584
Ayana Bando <ayanaflax@yahoo.com>
2018-05-15 13:58:40 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :isosceles, :equilateral, :scalene
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+
+    if illegal_triangle?
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    elsif length1 == length2 && length2 == length3
+    :equilateral
+    elsif
+      side1 == side2 || side2 == side3|| side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def illegal_triangle?
+  end
+
+  class TriangleError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+
 end
