4a4ef01764db77755f391ae61411f88a65b32ba3
Ayana Bando <ayanaflax@yahoo.com>
2018-05-15 14:08:44 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,6 +23,7 @@ class Triangle
 
   def illegal_triangle?
     if @length1 == 0 && @length2 == 0 && @length3 == 0
+    elsif @length1 <0 && @length2 < 0 && @length3 < 0
       begin
         raise TriangleError
       end
