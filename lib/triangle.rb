c00db440764b26a70751b6eae5d43757dc3dbbda
Ayana Bando <ayanaflax@yahoo.com>
2018-05-15 14:19:47 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
   end
 
   def illegal_triangle?
-    if [(@length1 + @length2) > @length3 && (@length1 + @length3) < @length2]
+    if [(@length1 + @length2) > @length3 || (@length1 + @length3) < @length2]
     elsif @length1 < 0 || @length2 < 0 || @length3 < 0
     elsif @length1 = 0 || @length2 = 0 || @length3 = 0
       begin
